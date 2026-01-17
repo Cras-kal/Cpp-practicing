0ed6fb8d897380e38ec0cbc2cdbcad3e3ed8a965
Brian Howell <brian0912.vallejo@gmail.com>
2026-01-17 23:10:51 +0800

    Add scoring and game loop enhancements
    added: 
    - scoring
    - more interactive game
    - fixed where computer picks non-existent choice
    - fixed segmentation issue

--- RockPaperScissorsGame.cpp
+++ RockPaperScissorsGame.cpp
@@ -11,15 +11,19 @@
 char gameChoiceRand();
 std::string translateChoice(char randChoice);
 std::string theWinner(char userChoice, char gameChoice);
-int delayThree();
+int yourScore;
+int compScore;
 
 int main(){
     int state = 1; //initialize
-
+    srand(time(0));
+    
     while (state == 1){
         char userChoice;
         char gameChoice = gameChoiceRand();
         char agreeOrNot;
+
+        std::cout << "\n\n\n========== Welcome to BATO BATO PICK game ==========\n\n\n";
 
         do { // do this then if the choice is not r or p or s then loop
             std::cout << "Rock = r, Paper = p, scissors = s\n";
@@ -40,72 +44,80 @@ int main(){
         std::cout << "Computer's choice: " << gameChoiceString << std::endl;
         
         // we need to compare
-        std::cout << "\n**THE WINNER**\n";
-        std::cout << "The winner is: " << theWinner(userChoice, gameChoice) << std::endl;
+        std::cout << "\nThe winner is: " << theWinner(userChoice, gameChoice) << std::endl;
+        std::cout << "\n\n*****SCORE*****\n";
+        std::cout << "You: " << yourScore << " | Computer: " << compScore << std::endl;
 
+        do {
         std::cout << "\nDo you wanna play again?(y/n) ";
         std::cin >> agreeOrNot;
+        
+            if (agreeOrNot == 'y'){
+                state = 1;
+                std::cout << "\n\nLet's play again\n";
+            } else if (agreeOrNot == 'n'){
+                std::cout << "\n*****Thank you for playing*****\n\n\n";
+                state = 0;
+            } else if (agreeOrNot != 'y' && agreeOrNot != 'n'){
+                std::cout << "\nINVALID INPUT\n";
+            }
 
-        if (agreeOrNot == 'y'){
-            state = 1;
-            std::cout << "\n\nLet's play again\n";
-        } else if (agreeOrNot == 'n'){
-            std::cout << "\n***Thank you for playing***\n";
-            state = 0;
-        }
+        } while (agreeOrNot != 'y' && agreeOrNot != 'n');
     }
     return 0;
 }
 
 char gameChoiceRand(){
-    
     char choice;
-    srand(time(0));
-    int randomChoice = (rand() & 3) + 1; // 1 = r (rock), 2 = p (paper), 3 = s (scissors)
+    int randomChoice = (rand() % 3) + 1; // 1 = r (rock), 2 = p (paper), 3 = s (scissors)
     
     switch(randomChoice){
-        case 1: return choice = 'r'; break; //rock
-        case 2: return choice = 'p'; break; // paper
-        case 3: return choice = 's'; break; // scissors
+        case 1: choice = 'r'; break; //rock
+        case 2: choice = 'p'; break; // paper
+        case 3: choice = 's'; break; // scissors
     }
-    return 0;
+    return choice;
 }
 
-std::string translateChoice(char randChoice){
+std::string translateChoice(char randChoice){ //translate choice into a readable string
+    std::string string;
+
     switch (randChoice){
-        case 'r': return "Rock"; break;
-        case 'p': return "Paper"; break;
-        case 's': return "Scissors"; break;
+        case 'r': string = "Rock"; break;
+        case 'p': string = "Paper"; break;
+        case 's': string = "Scissors"; break;
     }
-    return 0;
+    return string;
 }
 
-std::string theWinner(char userChoice, char gameChoice){
+std::string theWinner(char userChoice, char gameChoice){ //function in choosing the winner
+    std::string whoIsWinner;
+
     switch(userChoice){
         case 'r': // the case where user choose rock
         switch(gameChoice){
-            case 'r': return "DRAW!!!"; break;
-            case 'p': return "YOU"; break;
-            case 's': return "COMPUTER"; break;
+            case 'r': whoIsWinner =  "DRAW!!!"; break; //comp choose rock draw
+            case 'p': compScore++; whoIsWinner = "COMPUTER"; break;//comp choose paper you lose
+            case 's': yourScore++; whoIsWinner = "YOU"; break; //comp choose scissors you win
         }
         break;
 
         case 'p': // the case where user choose paper
         switch(gameChoice){ 
-            case 'r': return "YOU"; break;
-            case 'p': return "DRAW!!!"; break;
-            case 's': return "COMPUTER"; break;
+            case 'r': yourScore++; whoIsWinner = "YOU"; break; // comp choose rock you win
+            case 'p': whoIsWinner = "DRAW!!!"; break; // comp choose paper draw
+            case 's': compScore++; whoIsWinner = "COMPUTER"; break; // comp choose scissors you lose
         }
         break;
         
-        case 's': 
+        case 's':  // case where user choose scissors
         switch(gameChoice){
-            case 'r': return "COMPUTER"; break;
-            case 'p': return "YOU"; break;
-            case 's': return "DRAW!!!"; break;
+            case 'r': compScore++; whoIsWinner = "COMPUTER"; break; // comp choose rock you lose
+            case 'p': yourScore++; whoIsWinner = "YOU"; break; // comp choose paper you win
+            case 's': whoIsWinner = "DRAW!!!"; break; // comp choose scissors draw
         }
         break; // the case where user choose scissors
     }
-    return 0;
+    return whoIsWinner;
 }
 
